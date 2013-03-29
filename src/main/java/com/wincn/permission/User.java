df14012cba8ffad6b0509a6e29d1a861e6d1e41e
gefangshuai <gefangshuai@163.com>
2013-03-29 22:25:40 +0800

    添加了user表

---
+++ src/main/java/com/wincn/permission/User.java
@@ -0,0 +1,42 @@
+package com.wincn.permission;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "p_user")
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
