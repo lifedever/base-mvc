e7a596883bb159089f9f2de72b7bc7b9ccd00d1d
gefangshuai <gefangshuai@163.com>
2013-03-30 02:42:00 +0800

    用户注册

--- src/main/java/com/wincn/permission/user/bean/User.java
+++ src/main/java/com/wincn/permission/user/bean/User.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "p_user")
--- src/main/java/com/wincn/permission/user/web/UserController.java
+++ src/main/java/com/wincn/permission/user/web/UserController.java
@@ -1,14 +1,22 @@
 package com.wincn.permission.user.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.wincn.permission.user.bean.User;
+import com.wincn.permission.user.service.UserService;
+import com.wincn.permission.user.validator.UserValidator;
 
 @Controller
 public class UserController {
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserValidator userValidator;
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String regeditForm(User user, ModelMap model) {
@@ -17,7 +25,9 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String regedit(User user, ModelMap model) {
+	public String regedit(User user, BindingResult bindingResult, ModelMap model) throws Exception {
+		if (!userService.regeditUser(user))
+			userValidator.validate(user, bindingResult);
 		return "permission/user/signup";
 	}
 }
