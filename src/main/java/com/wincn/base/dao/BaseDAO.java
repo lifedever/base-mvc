43e648b560491cd11899436e3510b7cc6ac9780e
gefangshuai <gefangshuai@163.com>
2013-03-30 13:19:38 +0800

    删除黄叹号

--- src/main/java/com/wincn/base/dao/BaseDAO.java
+++ src/main/java/com/wincn/base/dao/BaseDAO.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
 
 /**
  * jpa的基类
--- src/main/java/com/wincn/permission/login/LoginController.java
+++ src/main/java/com/wincn/permission/login/LoginController.java
@@ -3,13 +3,11 @@ package com.wincn.permission.login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.wincn.permission.user.bean.User;
 import com.wincn.permission.user.service.UserService;
-import com.wincn.permission.user.validator.UserValidator;
 
 /**
  * 用于登录
