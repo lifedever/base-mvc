cf6487d0be6cb56bcf4a6d33891d5efe858add0f
gefangshuai <gefangshuai@163.com>
2013-04-02 20:10:41 +0800

    提交

--- src/test/java/com/wincn/base/utils/velocity/service/GenerateServiceTest.java
+++ src/test/java/com/wincn/base/utils/velocity/service/GenerateServiceTest.java
@@ -1,15 +1,15 @@
 package com.wincn.base.utils.velocity.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-import com.wincn.permission.user.dao.UserDAO;
 import com.wincn.permission.user.service.UserService;
 
-@ContextConfiguration(locations = { "/config/spring/test-config.xml" })
+@ContextConfiguration(locations = { "/config/spring/servlet-context.xml" })
 @ActiveProfiles(value = "test")
 public class GenerateServiceTest extends AbstractJUnit4SpringContextTests {
 	@Autowired
@@ -17,6 +17,6 @@ public class GenerateServiceTest extends AbstractJUnit4SpringContextTests {
 
 	@Test
 	public void testname() {
-		
+		Assert.assertNotNull(userService);
 	}
 }
